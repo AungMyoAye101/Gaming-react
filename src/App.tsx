fa0e790340573db8977364d7151b25568ea749f1
AungMyoAye101 <aungmyoaye101@gmail.com>
2024-12-09 18:40:44 +0630

    add coustom colors

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ function App() {
       <h1 className="font-zentry">Zentry</h1>
       <h1 className="font-general">General</h1>
       <h1 className="font-circular-web">Circular</h1>
-      <h1 className="font-robert-medium">robert medium</h1>
-      <h1 className="font-robert-regular">robert regular</h1>
-      <p>normal text</p>
+      <h1 className="font-robert-medium text-violet-300">robert medium</h1>
+      <h1 className="font-robert-regular text-blue-50">robert regular</h1>
+      <p className="text-blue-100">normal text</p>
     </section>
   );
 }
