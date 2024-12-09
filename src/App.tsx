2b6acd27c26c8f383d6e37e34f8d4411238aa880
AungMyoAye101 <aungmyoaye101@gmail.com>
2024-12-09 18:34:43 +0630

    add custom fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
 function App() {
-  return <div className="text-red-500 bg-green-500">hello</div>;
+  return (
+    <section>
+      <h1 className="font-zentry">Zentry</h1>
+      <h1 className="font-general">General</h1>
+      <h1 className="font-circular-web">Circular</h1>
+      <h1 className="font-robert-medium">robert medium</h1>
+      <h1 className="font-robert-regular">robert regular</h1>
+      <p>normal text</p>
+    </section>
+  );
 }
 
 export default App;
