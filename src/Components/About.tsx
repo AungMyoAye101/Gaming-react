b327f3a435cfbb0b6a523b878eaaf8d940996871
AungMyoAye101 <aungmyoaye101@gmail.com>
2024-12-11 19:18:40 +0630

    update scroll animation of about image

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -22,30 +22,30 @@ const About = () => {
     });
   });
   return (
-    <section className="min-h-screen w-screen  " id="about">
-      <div className="flex flex-col gap-4 items-center  py-10 relative">
+    <section className="min-h-screen w-screen relative " id="about">
+      <div className="flex flex-col gap-4 items-center    py-6 relative">
         <h2 className="text-sm uppercase font-general">Welcome to Zentry</h2>
-        <h1 className="font-zentry uppercase text-4xl text-center spiceal-font leading-[0.8]">
+        <h1 className="font-zentry uppercase text-4xl md:text-6xl text-center spiceal-font leading-[0.8]">
           Disc<b>o</b>ver the <br />
           world's <br /> largest shared <b>a</b>dventure
         </h1>
 
-        {/* image section */}
-        <div className="h-dvh w-screen" id="clip">
-          <div className="mask-clip-path about-image">
-            <img
-              src="/img/about.webp"
-              alt="background image"
-              className="absolute left-0 right-0 size-full bg-blue-400 object-cover "
-            />
-          </div>
-        </div>
-        <div className="absolute bottom-0 z-10 text-center font-circular-web">
+        <div className="absolute bottom-[-80dvh] text-center font-circular-web font-semibold md:text-xl  left-1/2 -translate-x-1/2">
           <p>The Game of Games begins—your life, now an epic MMORPG</p>
           <p className="text-gray-500">
             Zentry unites every player from countless games and platforms, both
             digital and physical, into a unified Play Economy
           </p>
+        </div>
+      </div>
+      {/* image section */}
+      <div className="h-dvh w-screen" id="clip">
+        <div className="mask-clip-path about-image bg-blue-400">
+          <img
+            src="/img/about.webp"
+            alt="background image"
+            className="absolute left-0 right-0 size-full  object-cover "
+          />
         </div>
       </div>
     </section>
